67ef253f5ac83ed0aa45686fc8933a513f636d56
Sam Milechman <sammilechman@gmail.com>
2014-04-10 10:33:28 -0700

    Add functionalities to Piece class: move_list, deltas, on_board?, enemy_occupied?

--- board.rb
+++ board.rb
@@ -1,8 +1,26 @@
 require_relative 'piece.rb'
 
 class Board
-  def initialize
+  attr_accessor :rows
+
+  def initialize(new = true)
+    @rows = Array.new(10) { Array.new(10) } if new
+  end
+
+  def set_pieces
 
   end
+
+  def [](pos)
+    x, y = pos
+    @rows[x][y]
+  end
+
+  def []=(pos, piece)
+    x, y = pos
+    @rows[x][y] = piece
+  end
+
+
 
 end
--- piece.rb
+++ piece.rb
@@ -1,7 +1,39 @@
 class Piece
-  def initialize(board, position, color)
+  attr_reader :color, :position
+  attr_accessor :position, :king
+
+  def initialize(board, position, color, king = false)
     @board = board
     @position = position
     @color = color
+    @king = king
   end
+
+  def move_list
+    list = []
+    self.deltas.each do |delta|
+      new_pos = [(delta[0] + @position[0]), (delta[1] + @position[1])]
+      list << new_pos if on_board?(new_pos)
+    end
+    list
+  end
+
+  def deltas
+    list = []
+    list << [ -1, -1 ] << [ -1, 1 ] if color == :white || king == true
+    list << [ 1, -1 ] << [ 1, 1 ] if color == :black || king == true
+    list
+  end
+
+  def on_board?(pos)
+    pos[0] >= 0 && pos[0] <= 9 && pos[1] >= 0 && pos[1] <= 9
+  end
+
+  def enemy_occupied?(pos)
+    enemy_color = (@color == :white) ? :black : :white
+    return false if @board[pos].nil?
+    @board[pos].color == enemy_color
+  end
+
+
 end
